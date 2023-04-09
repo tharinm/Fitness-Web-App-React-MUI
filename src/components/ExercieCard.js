0296692d655e15b3cad9c4f2861d199075158ca6
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-09 21:14:07 +0530

    fix some layout issue in exercise card component

--- src/components/ExercieCard.js
+++ src/components/ExercieCard.js
@@ -5,10 +5,45 @@ import { Button, Stack, Typography } from "@mui/material";
 export default function ExercieCard({ exercise }) {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
-          <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-          <Typography >
-              {exercise.name}
-          </Typography>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#FFA9A9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#FCC757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+          mt="5px"
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+      >
+        {exercise.name}
+      </Typography>
     </Link>
   );
 }
