3920c596cee0cd1a7fa014357c1a0e7b08bd0d66
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-10 18:53:59 +0530

    create single exercis post

---
+++ src/components/Details.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Typography, Stack, Button, capitalize } from "@mui/material";
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+export default function Details({ exerciseDetail }) {
+  const { bodyPart, gifURL, name, target, equipment } = exerciseDetail;
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: equipment,
+      name: equipment,
+    },
+  ];
+
+  return (
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifURL} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong.{name}{" "}
+          <span style={{ textTransform: "capitalize" }}>{name}</span> bup is one
+          of the best <br /> exercises to target your {target}. It will help you
+          improve your <br /> mood and gain energy.
+        </Typography>
+        {extraDetail.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            <Button
+              sx={{
+                background: "#fff2db",
+                borderRadius: "50%",
+                width: "100px",
+                height: "100px",
+              }}
+            >
+              <img
+                src={item.icon}
+                alt={bodyPart}
+                style={{ height: "50px", width: "50px" }}
+              />
+            </Button>
+            <Typography varient="h5" textTransform="capitalize">{item.name}</Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
